f9e145980ec1e2de27c2963d9020340b57424ad0
Thai <thaila71669@gmail.com>
2021-11-28 20:52:32 -0600

    Trying to add https

--- app.js
+++ app.js
@@ -5,7 +5,17 @@ const express = require("express");
 const path = require("path");
 const fs = require('fs');
 
+const key = fs.readFileSync("./selfsigned.key");
+const cert = fs.readFileSync("./selfsigned.crt");
 const app = express();
+
+const https = require("https");
+const http = require("http");
+
+const httpsServer = https.createServer({key: key, cert: cert}, app);
+const httpServer = http.createServer(app);
+
+
 app.use(bodyparser.urlencoded({extended: true}));
 
 const PORT = 3000;
@@ -86,8 +96,9 @@ app.use("/inquiry", inquiryRoute);
 
 
 app.use("/", express.static(path.join(__dirname, "./views")));
-
+// httpServer.listen(PORT, () => {console.log("http listening on port 3000")});
+httpsServer.listen(3001, () => {console.log("https secure listening on port 3001")});
 
 app.listen(PORT, HOST);
-console.log("Server alive\n");
+// console.log("Server alive\n");
 
